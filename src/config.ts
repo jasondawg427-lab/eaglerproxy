f2b2ea4290f7427e418eaa9bba968257deaccf52
jasondawg427-lab <jasondawg427@gmail.com>
2026-06-02 15:47:43 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -46,8 +46,8 @@ export const config: Config = {
       originBlacklist: null,
     },
     server: {
-      host: "127.0.0.1",
-      port: 1111,
+      host: "something-INAR.aternos.me",
+      port: 11586,
     },
     tls: undefined,
   },
